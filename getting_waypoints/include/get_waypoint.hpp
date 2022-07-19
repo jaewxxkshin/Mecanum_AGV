ec03e716b54b7bbf607f6112a6fa9644d938665f
jaewxxkshin <jaewxxkshin@naver.com>
2022-07-19 21:07:49 +0900

    getting_waypoints_v_1

---
+++ getting_waypoints/include/get_waypoint.hpp
@@ -0,0 +1,134 @@
+#include <ros/ros.h>
+#include <librealsense2/rs.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <numeric>
+#include <std_msgs/Int16MultiArray.h>
+
+
+// constant value[JH]
+#define left_width 310
+#define right_width 885
+#define img_height 720
+#define distance_of_pixel 0.075  //cm
+#define pixels 72
+#define PI 3.141592
+#define corner_threshold 700
+
+using namespace cv;
+using namespace std;
+
+// vector element initialization [W]
+void vec_delete(vector<int> &vec)
+{
+    vec.clear();
+    vector<int>().swap(vec);
+}
+
+void vec_delete_p(vector<Point> &vec)
+{
+    vec.clear();
+    vector<Point>().swap(vec);
+}
+
+void vec_delete_pair(vector<pair<int, int>> &vec)
+{
+    vec.clear();
+    vector<pair<int, int>>().swap(vec);
+}
+
+// function for coordinate conversion [JH]
+// -------------------------------------------
+int convert_x (int &x)
+{ 
+  int converted_x = 0;
+  converted_x= x-((right_width-left_width)/2 + left_width);
+  return converted_x;
+}
+
+int convert_y (int &y)
+{
+  int converted_y = 0;
+  converted_y= -(y-img_height);
+  return converted_y;
+}
+
+int inv_convert_x( const int &x)
+{
+  int inv_converted_x=0;
+  inv_converted_x = x+ ((right_width-left_width)/2 + left_width);
+  return inv_converted_x;
+}
+
+int inv_convert_y( const int &y)
+{
+  int inv_converted_y=0;
+  inv_converted_y = -y+img_height;
+  return inv_converted_y;
+}
+// -------------------------------------------
+
+// resize function [HW]
+void set_array(std_msgs::Int16MultiArray &arr, const int &n)
+{
+  arr.data.resize(n);
+  return;
+}
+
+// Matrix for img_proc [W]
+Mat src, dst, image, mask, mask_sc, res, hsv, points, labels, centers;
+
+// point for warp
+Point2f src_p[4], dst_p[4];
+
+const int cluster_k = 8;
+
+// vector for finding contour [W]
+vector<Vec4i> linesP, hierachy;
+vector<vector<Point>> contours; 
+
+// if system detect more than 2 contours -> connect every contours [JH]
+vector<Point> contours_sum;
+
+// vector for fitLine() function[W]
+Vec4f detected_line;
+// vector that contains all of contours's y value [W]
+vector<int> y_val,  x_val;
+
+// vector that contains waypoint's position [W]
+vector<int> wp_y;
+vector<int> wp_x;
+vector<int> bottom_x;
+
+// variation after applying fitLine() function [W]
+float vx, vy;
+
+// varaitons of kmeans algorithm
+int width, height,  x, y, n, nPoints, cIndex, iTemp;
+int upper_x, lower_x, top_y, converted_x, converted_y, right_x, left_x;
+
+// 2-D vector for way points [W]
+vector<pair<int, int>> wp_xy;
+
+// x, y positions [W]
+float x_pos, y_pos;
+
+// -------------------------------------------
+// Red_HSV_range (based on dataset)
+Scalar lower_red = Scalar(160, 100, 100);
+Scalar upper_red = Scalar(180, 255, 255);
+// Red_HSV_range (hsv of red - special case)
+Scalar lower_red_sc = Scalar(0, 100, 100);
+Scalar upper_red_sc = Scalar(20, 255, 255);
+// Blue_HSV_range (based on dataset_ 1)
+Scalar lower_blue = Scalar(90, 100, 100);
+Scalar upper_blue = Scalar(130, 255, 255);
+// -------------------------------------------
+
+// Image renewal flag variation [W]
+int countt = 0;
---
+++ getting_waypoints/src/get_waypoint.cpp
@@ -0,0 +1,274 @@
+#include "get_waypoint.hpp"
+
+using namespace cv;
+using namespace std;
+
+// select line which we want to tracking(by color) - demo [HW]
+int color = 1;
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "ros_realsense_opencv_tutorial");
+    ros::NodeHandle nh;
+    // ros::Publisher msg_hsv = nh.advertise<std_msgs::Int16MultiArray>("msg_hsv", 1000);  
+    // get camera info
+    rs2::pipeline pipe;
+    rs2::config cfg;
+    rs2::frameset frames;
+    rs2::frame color_frame;   
+    cfg.enable_stream(RS2_STREAM_COLOR, 1280,720, RS2_FORMAT_BGR8, 30);
+    pipe.start(cfg);    
+
+    // if it doesn't exist, it cause error [JH]
+    for(int i=0; i < 30; i ++)
+    {
+        frames = pipe.wait_for_frames();
+    }
+    // need to trouble shooting about ros loop rate
+    ros::Rate loop_rate(30);
+
+
+    while(ros::ok())
+    {
+
+        frames = pipe.wait_for_frames();
+        color_frame = frames.get_color_frame();
+        cout << "count : " << countt << endl;
+        //imshow("Display Image", src);
+
+        // perspective transform [JH]
+        src_p[0] = Point2f(443,478);
+        src_p[1] = Point2f(336,720);
+        src_p[2] = Point2f(855,474);
+        src_p[3] = Point2f(940,720);
+        
+        dst_p[0] = Point2f(604-268/2,720-268/2*3/2);
+        dst_p[1] = Point2f(604-268/2,720);
+        dst_p[2] = Point2f(604+268/2,720-268/2*3/2);
+        dst_p[3] = Point2f(604+268/2,720);
+        
+        Mat perspective_mat = getPerspectiveTransform(src_p, dst_p);
+
+        //get image at regular intervals[W]
+        if(countt== 0 || countt ==120) 
+        { 
+            // initailization of renewal flag variation[W]
+            countt=0;
+
+            // variations [W]
+            //------------------------------------------------------------------------------------------
+            // Image generation variation[W]
+            Mat src(Size(1280,720), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP);
+           
+            // perspective matrix [W]
+            warpPerspective(src, dst, perspective_mat, Size(1280,720));
+            //------------------------------------------------------------------------------------------        
+            // recognize image informations[W]
+            width = dst.cols, height = dst.rows;
+            nPoints = width * height;
+
+            // initialization (create)[W]
+            points.create(nPoints, 1, CV_32FC3);        // input data[W]
+            centers.create(cluster_k, 1, points.type());        // results of k means[W]
+            res.create(height, width, dst.type());      // results images[W]
+            
+            // data transform to fitting kmeasn algortihm[W]
+            // there's no way to save computing time????[W]
+            for(y = 0, n = 0; y < height; y++)
+            {
+                for(x = 0; x < width; x++, n++)
+                {
+                    points.at<Vec3f>(n)[0] = dst.at<Vec3b>(y, x)[0];
+                    points.at<Vec3f>(n)[1] = dst.at<Vec3b>(y, x)[1];
+                    points.at<Vec3f>(n)[2] = dst.at<Vec3b>(y, x)[2];
+                } 
+            }
+            
+            // k-means clustering[W]
+            kmeans(points, cluster_k, labels, TermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0), 
+            20, KMEANS_PP_CENTERS, centers);
+    
+            // visualization of result of kmeans algorithm[W]
+            // if We don't need to visualization ...??[W]
+            // test v_ 1 2022.07.12 [W]
+            //---------------------------------------------------------
+            for(y = 0, n = 0; y < height; y++)
+            {
+                for(x = 0; x < width; x++, n++)
+                {
+                    cIndex = labels.at<int>(n);
+                    iTemp = cvRound(centers.at<Vec3f>(cIndex)[0]);
+                    iTemp = iTemp > 255 ? 255 : iTemp < 0 ? 0 : iTemp;
+                    res.at<Vec3b>(y, x)[0] = (uchar)iTemp;
+
+                    iTemp = cvRound(centers.at<Vec3f>(cIndex)[1]);
+                    iTemp = iTemp > 255 ? 255 : iTemp < 0 ? 0 : iTemp;
+                    res.at<Vec3b>(y, x)[1] = (uchar)iTemp;
+
+                    iTemp = cvRound(centers.at<Vec3f>(cIndex)[2]);
+                    iTemp = iTemp > 255 ? 255 : iTemp < 0 ? 0 : iTemp;
+                    res.at<Vec3b>(y, x)[2] = (uchar)iTemp;
+                } 
+            }
+
+            // BGR -> HSV [HW]
+            cvtColor(res, hsv, COLOR_BGR2HSV);
+            
+            // HSV detect [W]
+            // mask : color 1 is red / color 2 is blue - demo version [W]
+            //========================================================================    
+            if(color ==1)
+            {
+              inRange(hsv, lower_red, upper_red, mask);  
+              inRange(hsv, lower_red_sc, upper_red_sc, mask_sc);
+              bitwise_or(mask, mask_sc, mask);
+              bitwise_and(res, res, image, mask);
+            }
+            else if (color ==2)
+            {
+              inRange(hsv, lower_blue, upper_blue, mask);  
+              bitwise_and(res, res, image, mask);
+            }
+            //========================================================================
+
+            // find contours [HW]
+            findContours(mask, contours, hierachy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE); 
+            drawContours(image, contours, -1, Scalar(255, 0, 0), 5);
+
+            // get contour's y_val( every y ), x_val (every x ) [JH]
+            for ( int i = 0; i < contours.size(); i++)
+            {
+                for ( int j = 0; j < contours[i].size(); j++)
+                {
+                    y_val.push_back(contours[i][j].y);
+                }
+            }
+
+            for ( int i = 0; i < contours.size(); i++)
+            {
+                for ( int j = 0; j < contours[i].size(); j++)
+                {
+                    x_val.push_back(contours[i][j].x);
+                }
+            }
+
+            // contour's x,y min/max value [JH]
+            int min = *min_element(y_val.begin(),y_val.end()); 
+            int max = *max_element(y_val.begin(),y_val.end()); 
+            int x_left = *min_element(x_val.begin(),x_val.end());
+            int x_right = *max_element(x_val.begin(),x_val.end());
+             
+            // contour's bottom x value [JH]
+            for ( int i = 0; i < contours.size(); i++)
+            {
+                for ( int j = 0; j < contours[i].size(); j++)
+                {
+                    if(contours[i][j].y == max)
+                    {
+                        bottom_x.push_back(contours[i][j].x);
+                    }
+                }
+            }
+
+            // get avg of contour's bottom x value [JH]
+            int sum_bottom_x = accumulate(bottom_x.begin(),bottom_x.end(),0);
+            int mean_bottom_x = sum_bottom_x/bottom_x.size();
+            
+            // if system detect more than 2 contours -> connect every contours [JH]
+            for (int i=0; i < contours.size(); i++)
+            {
+                for (int j=0; j < contours[i].size() ; j++)
+                {
+                    contours_sum.push_back(contours[i][j]);
+                }
+            }
+
+            // fitLine() function to detect representive line [W]
+            fitLine(contours_sum, detected_line, CV_DIST_L2, 0, 0.01, 0.01);
+            vx = detected_line[0];
+            vy = detected_line[1];
+            x = int(detected_line[2]);
+            y = int(detected_line[3]);
+
+            // get (x,y) of detected line in converted coordinate [JH]
+            converted_x = convert_x(x);
+            converted_y = convert_y(y);
+            top_y = convert_y(min);
+            left_x = convert_x(x_left);
+            right_x = convert_x(x_right);
+
+            // get the source of drawing straight line [JH]
+            upper_x = int(vx/vy*(top_y-converted_y)+converted_x);
+            lower_x = int(vx/vy*(-1*converted_y) + converted_x);
+            
+            // waypoint visualization [W]
+            if(top_y > corner_threshold) // go straight
+            {
+                for(int i=0; i<10; i++)
+                {
+                    wp_y.push_back(top_y/10*(i+1));
+                    wp_x.push_back((vx/vy*(wp_y[i]-converted_y)+converted_x));
+                    // use make_pair [W]
+                    wp_xy.push_back(make_pair(wp_x[i], wp_y[i]));
+                }
+                // visualization representive line [W]
+                line(res, Point(inv_convert_x(upper_x),inv_convert_y(top_y)), Point(inv_convert_x(lower_x),inv_convert_y(0)),Scalar(0,0,255), 3);
+            }
+            else if(top_y<corner_threshold) // turn right / left
+            {
+                for(int i=0; i<10; i++) // circle waypoint y
+                {
+                    float theta = (i+1) * 10 * PI / 180;
+                    wp_y.push_back(top_y * sin(theta));
+                }
+                if (vy/vx > 0) // turn left - waypoint x
+                {
+                    for(int i=0; i<10; i++)
+                    {
+                        float theta = (i+1)*10 * PI / 180;
+                        wp_x.push_back(convert_x(mean_bottom_x)- top_y + top_y * cos(theta));
+                    }
+                } 
+                else if (vy/vx < 0) // turn right - waypoint x
+                {
+                    for(int i=0; i<10; i++)
+                    {
+                        float theta = (i+1)*10 * PI / 180;
+                        wp_x.push_back(convert_x(mean_bottom_x) + top_y - top_y * cos(theta));
+                    }
+                }
+            }
+            for(int i=0; i<10; i++)
+            {
+                circle(res, Point(inv_convert_x(wp_x[i]), inv_convert_y(wp_y[i])), 5, Scalar(255,255,255), 3);
+            }
+            
+            // print x,y position of way points [W]
+            for(int i = 0; i < wp_xy.size(); i++)
+            {
+                x_pos = wp_xy[i].first*distance_of_pixel;
+                y_pos =  wp_xy[i].second*distance_of_pixel;
+                cout <<"way point" << i + 1<< "'s x value : " << x_pos << "cm, way point"
+                << i + 1 << "'s y value: " << y_pos<< "cm" <<endl;
+            }
+           
+            // vector initialization [W]
+            //----------------------------
+            vec_delete(x_val);
+            vec_delete(y_val);
+            vec_delete(wp_x);
+            vec_delete(wp_y);
+            vec_delete(bottom_x);
+            vec_delete_p(contours_sum);
+            vec_delete_pair(wp_xy);
+            //----------------------------
+        
+            imwrite("res.png", res);
+            // need trouble shooting [W]
+            loop_rate.sleep();
+            ros::spinOnce();
+        }
+        countt++;
+    }
+    return 0;
+}
